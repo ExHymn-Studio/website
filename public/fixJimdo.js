2ee7042142779045f275a08470d193dccd598403
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-04-26 12:12:36 +0200

    i am suffering

---
+++ public/fixJimdo.js
@@ -0,0 +1,137 @@
+document.querySelector("html").innerHTML = `<head>
+    <meta charSet="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+                <meta name="description" content="ExHymn Studio is a small game studio run by a few german students.">
+                    <title>ExHymn Studio</title>
+                    <script defer>(()=>{"use strict";var e={724:(e,t,o)=>{o.r(t)},25:()=>{function e(e){var t=document.getElementById(e+"-link"),o=document.getElementById(e);t.addEventListener("click",(function(e){e.preventDefault(),o.scrollIntoView({block:"start",behavior:"smooth",inline:"start"})}))}e("idea"),e("about")}},t={};function o(r){var n=t[r];if(void 0!==n)return n.exports;var i=t[r]={exports:{}};return e[r](i,i.exports,o),i.exports}o.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o(724),o(25)})();</script>
+                    <style>@import url(https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap);
+@import url(https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap);
+@import url(https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap);
+*{box-sizing:border-box}html,body{overflow-x:hidden}*,*:before,*:after{box-sizing:border-box}html,body,div,span,object,iframe,figure,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,code,em,img,small,strike,strong,sub,sup,tt,b,u,i,ol,ul,li,fieldset,form,label,table,caption,tbody,tfoot,thead,tr,th,td,main,canvas,embed,footer,header,nav,section,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;text-rendering:optimizeLegibility;-webkit-font-smoothing:antialiased;text-size-adjust:none}footer,header,nav,section,main{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:"";content:none}table{border-collapse:collapse;border-spacing:0}input{-webkit-appearance:none;border-radius:0}header{background-color:#1a1a1a;display:flex;justify-content:center;align-items:center;height:80vh}@media(orientation: landscape){header img{height:70vh}}@media(orientation: portrait){header img{width:70vw}}nav{background-color:#1a1a1a;height:10vh;display:flex;justify-content:center}nav a.el{padding:15px;color:#d7d7d7;font-size:2rem;text-decoration:none}nav a.el:hover{color:#e6e6e6;text-decoration:underline;cursor:pointer}.divider{box-sizing:border-box;border-style:solid;border-width:10vh 50vw;border-color:#1a1a1a transparent transparent;height:5vh;width:100%}html,body{min-height:100vh;background-color:#2a2a2a;font-family:"Roboto",Helvetica,Arial,sans-serif;color:#d7d7d7}main{margin:5rem 0}.content-block{max-width:850px;margin:0 25px}@media(min-width: 900px){.content-block{margin:0 auto}}h1{font-weight:normal;font-size:2.5rem;font-family:"Montserrat",sans-serif;margin-left:.5rem;margin-bottom:1.75rem}p{font-weight:normal;font-size:1.25rem;margin-bottom:2rem}footer{background-color:#1a1a1a;padding:2rem;width:100%;text-align:center}.card{border-radius:15px;margin:15px auto;background-color:transparent;width:300px;height:400px;perspective:1000px;filter:drop-shadow(0 10px 15px #0000003f)}@media(min-width: 330px){.card{margin:15px}}.card .inner{position:relative;width:100%;height:100%;text-align:center;transition:transform .8s;transform-style:preserve-3d}.card .inner .front,.card .inner .back{border-radius:15px;position:absolute;width:100%;height:100%;-webkit-backface-visibility:hidden;backface-visibility:hidden}.card .inner .front{background-color:#1a1a1a;color:#e6e6e6}.card .inner .front img{margin:50px 50px 50px;border-radius:50%;width:200px;height:200px}.card .inner .front h2{font-size:2.5rem}.card .inner .back{background-color:#1a1a1a;padding:50px;color:#d7d7d7;transform:rotateY(180deg)}.card:hover .inner{transform:rotateY(180deg)}.cards{margin:3rem 0;display:flex;justify-content:center;flex-wrap:wrap}</style>
+</head>
+<body>
+<header><svg
+   width="15.177771mm"
+   height="19.930246mm"
+   viewBox="0 0 15.177771 19.930247"
+   version="1.1"
+   id="svg932"
+   inkscape:version="1.1.2 (0a00cf5339, 2022-02-04, custom)"
+   sodipodi:docname="logo.svg"
+   inkscape:export-filename="/home/fells/bitmap.png"
+   inkscape:export-xdpi="600"
+   inkscape:export-ydpi="600"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <sodipodi:namedview
+     id="namedview934"
+     pagecolor="#505050"
+     bordercolor="#eeeeee"
+     borderopacity="1"
+     inkscape:pageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="0"
+     inkscape:document-units="mm"
+     showgrid="false"
+     inkscape:zoom="5.986532"
+     inkscape:cx="26.309055"
+     inkscape:cy="36.415073"
+     inkscape:window-width="1920"
+     inkscape:window-height="1158"
+     inkscape:window-x="0"
+     inkscape:window-y="42"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="g19972" />
+  <defs
+     id="defs929" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-98.128972,-94.49489)">
+    <g
+       id="g19972"
+       transform="translate(37.406079,15.046367)">
+      <text
+         xml:space="preserve"
+         style="font-size:3.52777px;line-height:4.03px;font-family:Amiri;-inkscape-font-specification:Amiri;letter-spacing:0px;word-spacing:0px;writing-mode:lr-tb;fill:#e6e6e6;fill-opacity:1;stroke-width:0.264583"
+         x="68.212715"
+         y="87.449509"
+         id="text1460"><tspan
+           sodipodi:role="line"
+           id="tspan1458"
+           style="font-size:12.3472px;text-align:center;text-anchor:middle;fill:#e6e6e6;fill-opacity:1;stroke-width:0.264583"
+           x="68.212715"
+           y="87.449509">EX</tspan><tspan
+           sodipodi:role="line"
+           style="font-size:6.35px;text-align:center;text-anchor:middle;fill:#e6e6e6;fill-opacity:1;stroke-width:0.264583"
+           x="68.212715"
+           y="91.872826"
+           id="tspan1462">Hymn</tspan></text>
+      <g
+         id="g19246"
+         transform="translate(0.2047018)">
+        <path
+           id="path13993"
+           style="fill:#e6e6e6;stroke:#e6e6e6;stroke-width:0.499998;stroke-linecap:square;stroke-linejoin:round"
+           d="m 68.093476,94.072963 a 2.5051123,2.5278293 0 0 0 -2.504833,2.527552 2.5051123,2.5278293 0 0 0 2.504833,2.528256 2.5051123,2.5278293 0 0 0 0.861168,-0.155931 2.5051123,2.5278293 0 0 1 -1.643665,-2.372325 2.5051123,2.5278293 0 0 1 1.646474,-2.372324 2.5051123,2.5278293 0 0 0 -0.863977,-0.155228 z" />
+        <path
+           id="path14087"
+           style="fill:#e6e6e6;stroke:#e6e6e6;stroke-width:0.499999;stroke-linecap:square;stroke-linejoin:round"
+           d="m 70.085391,95.019373 a 1.5662242,1.5814465 0 0 0 -1.566049,1.581274 1.5662242,1.5814465 0 0 0 1.566049,1.581713 1.5662242,1.5814465 0 0 0 0.538413,-0.09755 1.5662242,1.5814465 0 0 1 -1.027639,-1.48416 1.5662242,1.5814465 0 0 1 1.029395,-1.484161 1.5662242,1.5814465 0 0 0 -0.540169,-0.09711 z" />
+      </g>
+    </g>
+  </g>
+</svg></header>
+<nav><a className="el" href="#idea" id="idea-link">The idea</a><a className="el" href="#about" id="about-link">About
+    us</a></nav>
+<div className="divider"></div>
+<main>
+    <section id="idea">
+        <div className="content-block"><h1>Idea</h1><p>This site is currently in construction.</p></div>
+    </section>
+    <section id="about">
+        <div className="content-block"><h1>About us</h1>
+            <p>blablablablablablablablablablablablablablablablablablablablablabla</p><p>maybe some cards for the
+                ppl???</p></div>
+        <div className="cards">
+            <div className="card">
+                <div className="inner">
+                    <div className="front"><img
+                        src="https://cdn.discordapp.com/avatars/233286691744514069/de1bf6d6717750144500f8d3bb3a9d6b.png"
+                        alt="Robins Discord Profilbild"><h2>Robin</h2></div>
+                    <div className="back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="inner">
+                    <div className="front"><img
+                        src="https://cdn.discordapp.com/avatars/576666905926959125/af1650a3e6825f7d53c19c190b060f40.png"
+                        alt="Maxims Discord Profilbild"><h2>Maxim</h2></div>
+                    <div className="back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="inner">
+                    <div className="front"><img
+                        src="https://cdn.discordapp.com/avatars/409316079744188426/6878de02a1ad83b7beffeb2ca5bf667e.png"
+                        alt="Pauls Discord Profilbild"><h2>Paul</h2></div>
+                    <div className="back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="inner">
+                    <div className="front"><img
+                        src="https://cdn.discordapp.com/avatars/266187155406192640/25bfac085f48766bfae191abe3575c9a.png"
+                        alt="Thomas' Discord Profilbild"><h2>Thomas</h2></div>
+                    <div className="back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer id="footer"><p>This is a footer!</p></footer>
+</body>`;
